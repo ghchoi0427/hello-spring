479bd51be0a8338c467debce5428e5dca6d78951
ghchoi0427 <ghchoi0427@gmail.com>
2021-02-16 14:35:06 +0900

    회원 웹 기능 - 홈 화면, 등록, 조회

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,4 +14,7 @@ public class HelloController {
         model.addAttribute("data","spring!");
         return "hello";
     }
+
+
+
 }
