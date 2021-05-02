34d545a7c37d82cccbeb793df203a19ea9b5fdcb
ghchoi0427 <ghchoi0427@gmail.com>
2021-05-02 15:10:15 +0900

    fix: BoardType 이동

--- src/test/java/com/web/JpaMappingTest.java
+++ src/test/java/com/web/JpaMappingTest.java
@@ -1,7 +1,7 @@
 package com.web;
 
 import com.web.domain.Board;
-import com.web.domain.BoardType;
+import com.web.domain.enums.BoardType;
 import com.web.domain.User;
 import com.web.repository.BoardRepository;
 import com.web.repository.UserRepository;
@@ -9,7 +9,6 @@ import com.web.repository.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
